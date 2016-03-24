9d5144ba839629be7cb0733fa52ea385fc0ba832
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-24 20:57:24 +0200

    Add routes and modify router

--- app/Router.php
+++ app/Router.php
@@ -1,21 +1,35 @@
 <?php
 
 class Router {
-    private $routes ;
+
+    private $routes;
+
     public function __construct() {
-       $this->run();
+        $routesPath = ROOT . "/config/routes.php";
+        $this->routes = include $routesPath;
+        $this->run();
     }
 
     private function getURI() {
         if (!empty($_SERVER['REQUEST_URI'])) {
-         return substr(trim($_SERVER['REQUEST_URI']),5,  strlen($_SERVER['REQUEST_URI']));
+            return substr(trim($_SERVER['REQUEST_URI']), 5, strlen($_SERVER['REQUEST_URI']));
         }
     }
-    private function run(){
+
+    private function run() {
         $uri = $this->getURI();
-        
-        foreach ($this->routes as $uriPattern) {
-            echo "$uriPattern<br/>";
+
+        foreach ($this->routes as $uriPattern => $path) {
+            if (preg_match("~$uriPattern~", $uri)) {
+                $internalRoute = preg_replace("~$uriPattern~",$path ,$uri );
+                $segments = explode('/', $internalRoute);
+                $controllerName = ucfirst(array_shift($segments) . "Controller");
+                $actionName = "action" . ucfirst(array_shift($segments));
+                echo "<br/><pre>";
+                echo "Имя контроллера = " . $controllerName;
+                echo "<br/>Имя метода = " . $actionName;
+            }
         }
     }
+
 }
--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,7 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+return array(
+    'registration' => 'registration/add',
+    'weather' => 'weather/zaporizhia',
+    'postfeedback' => 'feedback/add',
+    'feedbacklist' => 'feedback/show'
+);
