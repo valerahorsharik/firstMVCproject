65fbe96580cd358208ef159c9f07a05a8c6277b6
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-27 00:21:30 +0200

    Create login form

--- views/registration/index.php
+++ views/registration/index.php
@@ -1,42 +1,30 @@
-<form class="form-signin" role="form">
-    <h2 class="form-signin-heading">Please sign in</h2>
-    <input type="email" class="form-control" placeholder="Email address" required autofocus>
-    <input type="password" class="form-control" placeholder="Password" required>
-    <button class="btn btn-lg btn-primary btn-block "  type="submit">Sign in</button>
-</form>
-<div class="row">
-    <div class="col-md-3">
-    </div>
-    <div class="col-md-6">
-        <form class="form-horizontal" role="form">
-            <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="inputPassword3" class="col-sm-2 control-label">Пароль</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"> Запомнить меня
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-default">Войти</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="col-md-3">
-    </div>
+<div class="text-center">
+    <h2>Регистрация</h2>
 </div>
+<form class="form-horizontal  ">
+    <div class="form-group ">
+        <label class="control-label col-xs-5" for="firstName">Name:</label>
+        <div class="col-xs-3">
+            <input type="text" class="form-control" id="Name" placeholder="Name">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-5" for="inputEmail">Email:</label>
+        <div class="col-xs-3">
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-5" for="inputPassword">Password:</label>
+        <div class="col-xs-3">
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+        </div>
+    </div>
+    <br />
+    <div class="form-group">
+        <div class="col-xs-offset-5 col-xs-6">
+            <input type="submit" class="btn btn-primary" value="Регистрация">
+            <input type="reset" class="btn btn-default" value="Очистить форму">
+        </div>
+    </div>
+</form>
