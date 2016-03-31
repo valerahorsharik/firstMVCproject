226c92d93918fdedf696d4e49b409fcd70739b7e
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-31 13:15:44 +0300

    Add weather parse

--- controllers/WeatherController.php
+++ controllers/WeatherController.php
@@ -7,7 +7,11 @@ class WeatherController {
 
     public function actionZaporizhia() {
         User::checkAuth();
-        Weather::weatherShow();
+        $text = file_get_contents('https://www.gismeteo.ua/weather-zaporizhzhya-78813/');
+        $weather = Weather::getWeather($text);
+        $img = Weather::getImg($text);
+        $state = Weather::getState($text);
+        $pressure = Weather::getPressure($text);
         $currentPage = "/weather/index.php";
         require_once ROOT . '/views/index.php';
     }
