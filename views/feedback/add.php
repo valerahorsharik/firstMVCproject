8d11cb99c3474539e6b9342983c0bc4a7c92771f
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-31 13:16:51 +0300

    Add default

--- views/feedback/add.php
+++ views/feedback/add.php
@@ -6,13 +6,13 @@
     <?php endif; ?>
 <form action="#"method="POST">
     <label for="name" >Name: </label>
-    <input type="text" name="author" id="author" value="<?php echo $author; ?>" placeholder="Name"><br/>
+    <input type="text" name="author" id="author" value="<?php echo $author; ?>" placeholder="Name" required autofocus><br/>
     <label for="email" >Email: </label>
-    <input type="email" name="email" id="email" value="<?php echo $email; ?>" placeholder="Email"><br/>
+    <input type="email" name="email" id="email" value="<?php echo $email; ?>" placeholder="Email" required><br/>
     <label for="header" >Title: </label>
-    <input type="text" name="header" id="header" value="<?php echo $header; ?>" placeholder="Title"><br/>
+    <input type="text" name="header" id="header" value="<?php echo $header; ?>" placeholder="Title" required><br/>
     <label for="content" >Feedback: </label>
-    <input type="text" name="feedback" id="feedback" value="<?php echo $feedback; ?>" placeholder="Content"><br/>
+    <input type="text" name="feedback" id="feedback" value="<?php echo $feedback; ?>" placeholder="Content" required><br/>
     <label for="captcha" >Captcha: </label>
     <img src="/mvc/app/captcha.php" />
     <input  type="text" name="captcha" id="captcha" />	<br/>
