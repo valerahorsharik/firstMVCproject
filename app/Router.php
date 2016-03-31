43b1bd4815b7e08dca5c59a60921efd2c113a285
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-31 14:03:44 +0300

    Small fix

--- app/Router.php
+++ app/Router.php
@@ -12,7 +12,7 @@ class Router {
 
     private function getURI() {
         if (!empty($_SERVER['REQUEST_URI'])) {
-            return trim($_SERVER['REQUEST_URI']);
+            return substr(trim($_SERVER['REQUEST_URI']),1  );
         }
     }
 
@@ -23,7 +23,6 @@ class Router {
             if (preg_match("~$uriPattern~", $uri)) {
                 $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
                 $segments = explode('/', $internalRoute);
-                array_shift($segments);
                 $controllerName = ucfirst(array_shift($segments) . "Controller");
                 $actionName = "action" . ucfirst(array_shift($segments));
                 $params = $segments;
