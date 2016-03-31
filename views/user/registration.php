56ad1cc28fbb4c321b8bb7f6520a014b78e7e154
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-31 13:19:01 +0300

    Fix

--- views/user/registration.php
+++ views/user/registration.php
@@ -27,37 +27,7 @@
             <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
         </div>
     </div>
-    <div class="form-group">
-        <label class="control-label col-xs-4">Дата рождения:</label>
-        <div class="col-xs-2">
-            <select class="form-control">
-                <option>Дата</option>
-            </select>
-        </div>
-        <div class="col-xs-2">
-            <select class="form-control">
-                <option>Месяц</option>
-            </select>
-        </div>
-        <div class="col-xs-2">
-            <select class="form-control">
-                <option>Год</option>
-            </select>
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-xs-5">Пол:</label>
-        <div class="col-xs-1">
-            <label class="radio-inline">
-                <input type="radio" name="gender" value="male"> Male
-            </label>
-        </div>
-        <div class="col-xs-1">
-            <label class="radio-inline">
-                <input type="radio" name="gender" value="female"> Female
-            </label>
-        </div>
-    </div>
+    
     <br />
     <div class="form-group">
         <div class="col-xs-offset-5 col-xs-6">
